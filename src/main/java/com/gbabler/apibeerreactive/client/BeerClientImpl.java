a904820510a45004e203250e11bab2b6054b66ea
Babler <gabrielbabler@yahoo.com.br>
2021-10-27 23:32:08 -0300

    add annotations of Spring and add new class dependency

--- src/main/java/com/gbabler/apibeerreactive/client/BeerClientImpl.java
+++ src/main/java/com/gbabler/apibeerreactive/client/BeerClientImpl.java
@@ -2,12 +2,19 @@ package com.gbabler.apibeerreactive.client;
 
 import com.gbabler.apibeerreactive.model.BeerDTO;
 import com.gbabler.apibeerreactive.model.BeerPagedList;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final WebClient webClient;
 
     @Override
     public Mono<BeerDTO> getBeerById(UUID id, Boolean showInventoryOnHand) {
