6e6ee20fec18720afb612e48d7adce3ebe886917
Babler <gabrielbabler@yahoo.com.br>
2021-10-27 23:04:37 -0300

    base project

---
+++ src/main/java/com/gbabler/apibeerreactive/ApiBeerReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.gbabler.apibeerreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBeerReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBeerReactiveApplication.class, args);
+	}
+
+}
