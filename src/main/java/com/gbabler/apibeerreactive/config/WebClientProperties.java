b3195eaca1f996202ea83cef946c784231b7b096
Babler <gabrielbabler@yahoo.com.br>
2021-10-27 23:40:15 -0300

    add new propertie

--- src/main/java/com/gbabler/apibeerreactive/config/WebClientProperties.java
+++ src/main/java/com/gbabler/apibeerreactive/config/WebClientProperties.java
@@ -2,4 +2,5 @@ package com.gbabler.apibeerreactive.config;
 
 public class WebClientProperties {
     public static final String BASE_URL = "http://api.springframework.guru";
+    public static final String BEER_V1_PATH = "/api/v1/beer";
 }
