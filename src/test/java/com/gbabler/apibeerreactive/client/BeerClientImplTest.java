0038e88b594f4eb7d76abce909078565bdfbda36
Babler <gabrielbabler@yahoo.com.br>
2021-10-27 23:46:26 -0300

    add query params optionals into uri

--- src/test/java/com/gbabler/apibeerreactive/client/BeerClientImplTest.java
+++ src/test/java/com/gbabler/apibeerreactive/client/BeerClientImplTest.java
@@ -21,15 +21,25 @@ class BeerClientImplTest {
     }
 
     @Test
-    void listBeers() {
-        Mono<BeerPagedList> beerPagedListMono = beerClient.listBeers(null,null,null,null,
+    void listBeersPageSize10() {
+        Mono<BeerPagedList> beerPagedListMono = beerClient.listBeers(1,10,null,null,
                 null);
 
         BeerPagedList pagedList = beerPagedListMono.block();
 
         assertThat(pagedList).isNotNull();
-        assertThat(pagedList.getContent().size()).isGreaterThan(0);
-        System.out.println(pagedList.toList());
+        assertThat(pagedList.getContent().size()).isEqualTo(10);
+    }
+
+    @Test
+    void listBeersNoRecords() {
+        Mono<BeerPagedList> beerPagedListMono = beerClient.listBeers(10,20,null,null,
+                null);
+
+        BeerPagedList pagedList = beerPagedListMono.block();
+
+        assertThat(pagedList).isNotNull();
+        assertThat(pagedList.getContent().size()).isEqualTo(0);
     }
 
     @Test
