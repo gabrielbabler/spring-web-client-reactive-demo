36e09a6ba8f3afeec9c7cbcf140ee4b43716b2d8
Babler <gabrielbabler@yahoo.com.br>
2021-10-28 23:20:10 -0300

    implementation of updateBeer

--- src/main/java/com/gbabler/apibeerreactive/client/BeerClientImpl.java
+++ src/main/java/com/gbabler/apibeerreactive/client/BeerClientImpl.java
@@ -54,12 +54,17 @@ public class BeerClientImpl implements BeerClient {
     }
 
     @Override
-    public Mono<ResponseEntity> updateBeer(BeerDTO beerDto) {
-        return null;
+    public Mono<ResponseEntity<Void>> updateBeer(UUID beerId, BeerDTO beerDto) {
+        return webClient.put()
+                .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH_GET_BY_ID)
+                        .build(beerId))
+                .body(BodyInserters.fromValue(beerDto))
+                .retrieve()
+                .toBodilessEntity();
     }
 
     @Override
-    public Mono<ResponseEntity> deleteBeerById(UUID id) {
+    public Mono<ResponseEntity<Void>> deleteBeerById(UUID id) {
         return null;
     }
 
